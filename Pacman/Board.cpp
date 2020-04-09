8025c711da022b1a54208370d0020c984b6e3c4b
Youssef Agiza <youssefagiza@aucegypt.edu>
2020-04-09 23:49:12 +0200

    Implements Board class. Methods:-       - constructor       - setTextures(int ** arr) // sets textures according to the array       -drawOnWindow(sf::RenderWindow&) //draws board on window

--- Pacman/Board.cpp
+++ Pacman/Board.cpp
@@ -1,1 +1,69 @@
 #include "Board.h"
+namespace PacmanCS {
+	const int DEFAULT_ROW = 10;
+	const int DEFAULT_COL = 10;
+	const int DEFAULT_WALL = -1;
+//	Board::Board():mRows(DEFAULT_ROW),mCols(DEFAULT_COL){}
+
+	Board::Board(int rows=DEFAULT_ROW, int cols=DEFAULT_COL, std::string wall="", std::string road )
+		:mRows(rows), mCols(cols)
+	{
+		//validate later
+		mWallTexture.loadFromFile(wall);
+		mRoadTexture.loadFromFile(road);
+		mShape = new sf::RectangleShape*[mRows];
+		for (int i = 0; i < mRows; i++)
+		{
+			mShape[i] = new sf::RectangleShape[mCols];
+		}
+	}
+	void Board::drawOnWindow(sf::RenderWindow& w)
+	{//needs validation: check that shape is not a nullpointer
+		for (int i = 0; i < mRows; i++)
+			for(int j=0;j<mCols;j++)
+			w.draw((this->mShape[i][j]));
+
+	}
+	void Board::setTextures( int** arr)
+	{//needs validation to check if the size of arr == the size of mRows,mCols
+	// and check the pointers are not null
+		for (int i = 0; i < mRows; i++)
+			for (int j = 0; j < mCols; j++)
+			{
+				mShape[i][j].setPosition(50.0f + 70 * j, 50.0f + 70 * i);
+				mShape[i][j].setSize(sf::Vector2f(70.0f, 70.0f));
+				if (arr[i][j] == DEFAULT_WALL)
+				{
+					mShape[i][j].setTexture(&(this->mWallTexture));
+					mShape[i][j].setFillColor(sf::Color::Blue);
+				}
+				else
+				{
+					mShape[i][j].setTexture(&(this->mRoadTexture));
+					mShape[i][j].setFillColor(sf::Color::Black);
+				}
+			}
+	
+	
+	}
+	//void Board::setTextures(const int arr[][10])
+	//{
+	//	for (int i = 0; i < mRows; i++)
+	//		for (int j = 0; j < mCols; j++)
+	//		{
+	//			mShape[i][j].setPosition(50.0f + 70 * j, 50.0f + 70 * i);
+	//			mShape[i][j].setSize(sf::Vector2f(70.0f, 70.0f));
+	//			if (arr[i][j] == DEFAULT_WALL)
+	//			{
+	//				mShape[i][j].setTexture(&(this->mWallTexture));
+	//				mShape[i][j].setFillColor(sf::Color::Blue);
+	//			}
+	//			else
+	//			{
+	//				mShape[i][j].setTexture(&(this->mRoadTexture));
+	//				mShape[i][j].setFillColor(sf::Color::Black);
+	//			}
+	//		}
+	//}
+
+}
--- Pacman/main.cpp
+++ Pacman/main.cpp
@@ -1,24 +1,79 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Player.h"
+#include "Board.h"
+using namespace std;
+using namespace sf;
+using namespace PacmanCS;
+/************************************************************************/
+
+
+/************************************************************************/
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	const int ROWS = 11, COLUMNS = 11;
+	RenderWindow window;
+	window.create(VideoMode(1000, 1000), "Simple Maze");
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	int** arr;
+	arr = new int* [ROWS];
+	
+	for (int i=0; i< COLUMNS;i++)
+	{
+		arr[i] = new int[COLUMNS];
+	}
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+ifstream inputFile;
+	inputFile.open("BoardText.txt");
+	if (inputFile.is_open())
+	{
+		for (int i = 0; i < ROWS; i++)
+			for (int j = 0; j < COLUMNS; j++)
+				if (!inputFile.eof())
+					inputFile >> arr[i][j];
+	}
+	inputFile.close();
+	Player player(1, 1, "images/Pacman.png");
 
+	Board myBoard(ROWS,COLUMNS, "images/Block.png", "images/grass_tiled.png");
+	myBoard.setTextures(arr);
+
+	
+	Event e;
+	while (window.isOpen())
+	{
+		while (window.pollEvent(e))
+		{
+			if (e.type == Event::Closed)
+				window.close();
+			else if (e.type == Event::KeyPressed)
+			{
+				switch (e.key.code)
+				{
+				case Keyboard::Up:
+					player.move('U', arr);
+					break;
+				case Keyboard::Down:
+					player.move('D', arr);
+					break;
+				case Keyboard::Right:
+					player.move('R', arr);
+					break;
+				case Keyboard::Left:
+					player.move('L', arr);
+					break;
+				}
+			}
+		}
+		window.clear();
+
+		myBoard.drawOnWindow(window);
+		player.drawOnWindow(window);
+		window.display();
+	}
 	return 0;
+
 }
